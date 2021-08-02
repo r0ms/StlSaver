44ee7d38eff00465d574f7125ba51b10d7a6a071
Romain Bontemps <romain@ponteilla.net>
2021-08-02 18:49:14 +0200

    Update herosaver_test.js
    Test using local three.min.js

--- herosaver_test.js
+++ herosaver_test.js
@@ -292,7 +292,7 @@ function inject_script(url, callback) {
 console.log("HeroSaver - Injecting Libraries");
 
 inject_script("//code.jquery.com/jquery-3.3.1.min.js", function () {
-   inject_script("//cdn.jsdelivr.net/gh/burndaflame/three.js@dev/build/three.module.js", function () { init() })//
+   inject_script("//raw.githubusercontent.com/r0ms/StlSaver/master/three.min.js", function () { init() })
 });
 // inject_script("//code.jquery.com/jquery-3.3.1.min.js", function () {
 //     inject_script("//raw.githubusercontent.com/Sonic-Cloud/StlSaver/master/three.js", function () { init() })
