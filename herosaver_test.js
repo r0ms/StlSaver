38c1c3de654923833f37421da361d4e9002de755
Romain Bontemps <romain@ponteilla.net>
2021-08-02 19:38:01 +0200

    Update herosaver_test.js

--- herosaver_test.js
+++ herosaver_test.js
@@ -146,8 +146,7 @@ function init() {
 
 								const skeleton = this.skeleton;
 								const geometry = this.geometry;
-								debugger;
-								skinIndex.fromBufferAttribute( geometry.attributes.skinIndex0, index );
+								skinIndex.fromBufferAttribute( geometry.attributes.skin0, index );
 								skinWeight.fromBufferAttribute( geometry.attributes.skinWeight0, index );
 
 								basePosition.fromBufferAttribute( geometry.attributes.position, index ).applyMatrix4( this.bindMatrix );
